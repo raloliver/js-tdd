6bd80e39f99abda5bd857e88e25894ef0ccaa5f5
Israel Oliveira <raloliver.com@gmail.com>
2019-10-16 15:27:44 -0300

    Essential function off mocha

---
+++ src/main.spec.js
@@ -0,0 +1,22 @@
+describe('Main', function () {
+    describe('Method 0', function () {
+        context('Case A', function () {
+            it('should happen something...', function() {
+                //waint to happens
+                //input (method)
+                //output (return)
+            });
+        });
+        context('Case B', function () {
+
+        });
+    });
+    describe('Method 1', function () {
+        context('Case A', function () {
+
+        });
+        context('Case B', function () {
+
+        });
+    });
+});  
