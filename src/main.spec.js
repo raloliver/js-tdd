6c8ce1d61e8057fad66555e9cfe2ade540baf622
Israel Oliveira <raloliver.com@gmail.com>
2019-10-16 15:43:28 -0300

    context.only and it.skip

--- src/main.spec.js
+++ src/main.spec.js
@@ -1,22 +1,20 @@
 describe('Main', function () {
     describe('Method 0', function () {
         context('Case A', function () {
-            it('should happen something...', function() {
+            it('should happen on Case A in Method 0)', function() {
                 //waint to happens
                 //input (method)
                 //output (return)
+                throw new Error('Ops... ERROR!');
             });
         });
         context('Case B', function () {
-
+            it.skip('should happen on Case B in Method 0 [0])', function() {});
+            it('should happen on Case B in Method 0 [1])', function() {});
         });
     });
     describe('Method 1', function () {
-        context('Case A', function () {
-
-        });
-        context('Case B', function () {
-
-        });
+        // context.only('Case A', function () {});
+        context('Case B', function () {});
     });
 });  
