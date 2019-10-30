cfbecd56e6dd66fecd6e139cd37e65d989fc3a90
Israel Oliveira <raloliver.com@gmail.com>
2019-10-29 21:25:45 -0300

    Init test to fizzbuzz challenge

--- src/main.spec.js
+++ src/main.spec.js
@@ -1,58 +1,16 @@
+/*
+    FizzBuzz
+    Escreva uma função que receba um número e:
+    Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
+    Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
+    Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
+    Se não for múltiplo de nada, retorna o número
+ */
+
 import { expect } from 'chai';
-import { sum, sub, mult, div } from './main';
 
-describe('Calc', () => {
-
-    //init with smoke tests    
-    describe('Smoke test', () => {
-
-        it('should exits the `sum` method', () => {
-            expect(sum).to.exist;
-            expect(sum).to.be.a('function');
-        });
-
-        it('should exits the `sub` method', () => {
-            expect(sub).to.exist;
-            expect(sub).to.be.a('function');
-        });
-
-        it('should exits the `mult` method', () => {
-            expect(mult).to.exist;
-            expect(mult).to.be.a('function');
-        });
-
-        it('should exits the `div` method', () => {
-            expect(div).to.exist;
-            expect(div).to.be.a('function');
-        });
-
-    });
-
-    //check methods
-    describe('Sum', () => {
-        it('should return 4 when `sum(2,2)`', () => {
-            expect(sum(2, 2)).to.be.equal(4);
-        });
-    });
-
-    describe('Sub', () => {
-        it('should return 4 when `sub(6,2)`', () => {
-            expect(sub(6, 2)).to.be.equal(4);
-        });
-        it('should return -4 when `sub(6,10)`', () => {
-            expect(sub(6, 10)).to.be.equal(-4);
-        });
-    });
-
-    describe('Mult', () => {
-        it('should return 4 when `mult(2,2)`', () => {
-            expect(mult(2, 2)).to.be.equal(4);
-        });
-    });
-
-    describe('Div', () => {
-        it('should return `not divided by zero` when divide by zero', () => {
-            expect(div(4, 0)).to.be.equal('not divided by zero');
-        });
+describe('Main', () => {
+    it('should return `Fizz` when multiple by 3', () => {
+        expect(FizzBuzz(3)).to.be.equal('Fizz');
     });
 });
