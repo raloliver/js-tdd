cb54ff3fedd399997ec228b6eb35acf2b97a3054
Israel Oliveira <raloliver.com@gmail.com>
2019-10-29 21:34:31 -0300

    Fizz and Buzz its ok

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-const sum = (num1, num2) => num1 + num2;
-const sub = (num1, num2) => num1 - num2;
-const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => (num2 === 0) ? 'not divided by zero' : num1 / num2;
+const FizzBuzz = (num) => {
+    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+    if (num % 3 === 0) return 'Fizz';
+    if (num % 5 === 0) return 'Buzz';
+};
 
-export { sum, sub, mult, div }
+export default FizzBuzz;
--- src/main.spec.js
+++ src/main.spec.js
@@ -1,16 +1,27 @@
 /*
     FizzBuzz
     Escreva uma função que receba um número e:
-    Se o número for divisível por 3, no lugar do número escreva 'Fizz' - X
-    Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
-    Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
+    Se o número for divisível por 3, no lugar do número escreva 'Fizz'
+    Se o número for divisível por 5, no lugar do número escreva 'Buzz'
+    Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz'
     Se não for múltiplo de nada, retorna o número
  */
 
 import { expect } from 'chai';
+import FizzBuzz from './main';
 
 describe('Main', () => {
     it('should return `Fizz` when multiple by 3', () => {
         expect(FizzBuzz(3)).to.be.equal('Fizz');
+        expect(FizzBuzz(6)).to.be.equal('Fizz');
+    });
+
+    it('should return `Fizz` when multiple by 5', () => {
+        expect(FizzBuzz(5)).to.be.equal('Buzz');
+        expect(FizzBuzz(10)).to.be.equal('Buzz');
+    });
+
+    it('should return `FizzBuzz` when multiple by 3 and 5', () => {
+        expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
     });
 });
