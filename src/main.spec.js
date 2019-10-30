02aa9e638e6462598b03f84bbf5ec1def6e2d7f2
Israel Oliveira <raloliver.com@gmail.com>
2019-10-29 21:12:41 -0300

    Refactor main test and export/import correctly

--- src/main.spec.js
+++ src/main.spec.js
@@ -1,33 +1,29 @@
 import { expect } from 'chai';
-import calc from './main';
+import { sum, sub, mult, div } from './main';
 
 describe('Calc', () => {
 
     //init with smoke tests    
     describe('Smoke test', () => {
 
-        it('should exits the `calc` lib', () => {
-            expect(calc).to.exist;
-        });
-
         it('should exits the `sum` method', () => {
-            expect(calc.sum).to.exist;
-            expect(calc.sum).to.be.a('function');
+            expect(sum).to.exist;
+            expect(sum).to.be.a('function');
         });
 
         it('should exits the `sub` method', () => {
-            expect(calc.sub).to.exist;
-            expect(calc.sub).to.be.a('function');
+            expect(sub).to.exist;
+            expect(sub).to.be.a('function');
         });
 
         it('should exits the `mult` method', () => {
-            expect(calc.mult).to.exist;
-            expect(calc.mult).to.be.a('function');
+            expect(mult).to.exist;
+            expect(mult).to.be.a('function');
         });
 
         it('should exits the `div` method', () => {
-            expect(calc.div).to.exist;
-            expect(calc.div).to.be.a('function');
+            expect(div).to.exist;
+            expect(div).to.be.a('function');
         });
 
     });
@@ -35,28 +31,28 @@ describe('Calc', () => {
     //check methods
     describe('Sum', () => {
         it('should return 4 when `sum(2,2)`', () => {
-            expect(calc.sum(2, 2)).to.be.equal(4);
+            expect(sum(2, 2)).to.be.equal(4);
         });
     });
 
     describe('Sub', () => {
         it('should return 4 when `sub(6,2)`', () => {
-            expect(calc.sub(6, 2)).to.be.equal(4);
+            expect(sub(6, 2)).to.be.equal(4);
         });
         it('should return -4 when `sub(6,10)`', () => {
-            expect(calc.sub(6, 10)).to.be.equal(-4);
+            expect(sub(6, 10)).to.be.equal(-4);
         });
     });
 
     describe('Mult', () => {
         it('should return 4 when `mult(2,2)`', () => {
-            expect(calc.mult(2, 2)).to.be.equal(4);
+            expect(mult(2, 2)).to.be.equal(4);
         });
     });
 
     describe('Div', () => {
         it('should return `not divided by zero` when divide by zero', () => {
-            expect(calc.div(4, 0)).to.be.equal('not divided by zero');
+            expect(div(4, 0)).to.be.equal('not divided by zero');
         });
     });
 });
