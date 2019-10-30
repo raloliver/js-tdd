bdae078a6693822f2b6abed9990efde15379e78c
Israel Oliveira <raloliver.com@gmail.com>
2019-10-29 22:29:40 -0300

    Fixed lint on main file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 const FizzBuzz = (num) => {
-    if (num === 0) return 0;
-    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-    if (num % 3 === 0) return 'Fizz';
-    if (num % 5 === 0) return 'Buzz';
-    //if dont pass here 👆 return num 
-    return num;
+  if (num === 0) return 0;
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
+  if (num % 3 === 0) return 'Fizz';
+  if (num % 5 === 0) return 'Buzz';
+  // if dont pass here 👆 return num
+  return num;
 };
 
 export default FizzBuzz;
+git
